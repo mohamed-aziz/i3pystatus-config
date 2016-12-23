30d152e7aa171cae7fd5c5b820a00d3039404406
mohamed-aziz <medazizknani@gmail.com>
2016-12-23 14:41:09 +0000

    Refactored some stuff.

--- statusconfig.py
+++ statusconfig.py
@@ -14,7 +14,6 @@ from i3pystatus import Status
 from i3pystatus.core.command import run_through_shell
 from i3pystatus import IntervalModule
 from shutil import which
-
 status = Status()
 
 # Displays clock like this:
@@ -69,47 +68,10 @@ status.register("pulseaudio",
                 format_muted=" {volume}"
 )
 
-# this is weird but xbacklight does not work for me.
-class MyBacklight(IntervalModule):
-
-    settings = (
-        ('format', 'the format used to display backlight'),
-        ('step', 'The step of increase or decrease'),
-        ('icon', 'Icon to decorate'),
-    )
-
-    format = "{icon}{percentage}{symbole}"
-    step = 5
-    interval = 5
-    icon = ''
-    on_upscroll = "lighter"
-    on_downscroll = "darker"
-
-    has_light = which('light') is not None
-
-
-    def run(self):
-        if self.has_light:
-            self.percentage = run_through_shell(['light']).out.splitlines()[0]
-            self.output = {
-                'full_text': self.format.format(**{
-                    'percentage': self.percentage,
-                    'symbole': '%',
-                    'icon': self.icon
-                })
-            }
-    
-    def lighter(self):
-        if self.has_light:
-            run_through_shell(["light", "-A", str(self.step)])
-
-    def darker(self):
-        if self.has_light:
-            run_through_shell(["light", "-U", str(self.step)])
 
 
 status.register(
-    MyBacklight,
+    "backlight_light",
     format='{icon}  {percentage}{symbole}',
     step=5,
     icon='',
@@ -117,7 +79,8 @@ status.register(
 
 
 status.register(
-    "cmus"
+    "mpd",
+    format="{title} {status}"
 )
 
 
